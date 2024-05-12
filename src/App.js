65586640dd2b21a072397611208e160874787704
krish7-dev <krishnaanilkumar.dev@gmail.com>
2024-05-12 23:51:05 +0530

    hangman_first-deploy

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Routes,BrowserRouter as Router,Route } from "react-router-dom";
 import Home from './Home'
 import Minesweeper from './minesweeper/Minesweeper'
+import Hangman from './hangman/Hangman';
 function App() {
   return (
     <Router>
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/minesweeper" element={<Minesweeper/>} />
+          <Route exact path="/hangman" element={<Hangman/>} />
         </Routes>
     </Router>
   );
